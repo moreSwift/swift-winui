1695ee3ea2b7a249f6504c7f1759e7ec7a38eb86
stackotter <stackotter@stackotter.dev>
2025-10-17 22:44:53 +1000

    Update all dependencies (and transitive deps thereof) to robust exact revisions

--- Package.swift
+++ Package.swift
@@ -8,22 +8,25 @@ let package = Package(
         .library(name: "WinUI", type: .dynamic, targets: ["WinUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/thebrowsercompany/swift-cwinrt", branch: "main"),
+        .package(
+            url: "https://github.com/thebrowsercompany/swift-cwinrt",
+            revision: "eb46cdb66f770a1e006f9fcfebbf9e99a0fba811"
+        ),
         .package(
             url: "https://github.com/stackotter/swift-uwp",
-            branch: "c9d3fc079aaaa5113cde9a0132278fb83e808599"
+            revision: "e3ff9c195775e16b404b82cf6886c5e81d73b6c1"
         ),
         .package(
             url: "https://github.com/stackotter/swift-windowsappsdk",
-            branch: "ed938db0b9790b36391dc91b20cee81f2410309f"
+            revision: "ba6f0ec377b70d8be835d253102ff665a0e47d99"
         ),
         .package(
-            url: "https://github.com/thebrowsercompany/swift-windowsfoundation",
-            branch: "main"
+            url: "https://github.com/stackotter/swift-windowsfoundation",
+            revision: "4ad57d20553514bcb23724bdae9121569b19f172"
         ),
         .package(
             url: "https://github.com/stackotter/swift-webview2core",
-            branch: "9afd97424f844478914ca4512c8ca0a2d3a2bb67"
+            revision: "c9911ca23455b9fcdb2429e98baa6f4d003b381c"
         ),
     ],
     targets: [
